9634c72af312ffc6933462f3a3e9c3cd81a1f8bc
sarthakpranesh <sarthak.pranesh2018@vitstudent.ac.in>
2020-05-21 16:26:37 +0530

    Neural Network complete - epochs added - works - need to add documentation

--- cuslib/nn.go
+++ cuslib/nn.go
@@ -23,6 +23,8 @@ type NeuralNetwork struct {
 
 	learningRate		float64
 	activationFunc		*Activation // lol super shady
+
+	epochs				int // num of epochs to loop
 }
 
 /*
@@ -39,7 +41,7 @@ func newSigmoid() *Activation {
 	}
 }
 
-func NewNN(inputNodes, hiddenNodes, outputNodes int, learningRate float64, activationFunc string) *NeuralNetwork {
+func NewNN(inputNodes, hiddenNodes, outputNodes int, learningRate float64, activationFunc string, e int) *NeuralNetwork {
 	var actFunc *Activation
 	if activationFunc == "sigmoid" || activationFunc == "sgd" {
 		actFunc = newSigmoid()
@@ -55,17 +57,96 @@ func NewNN(inputNodes, hiddenNodes, outputNodes int, learningRate float64, activ
 		biasHO:         NewRandom(outputNodes, 1),
 		learningRate:   learningRate,
 		activationFunc: actFunc,
+		epochs:			e,
 	}
 }
 
-func (n *NeuralNetwork) FeedForward(input []float64) Matrix{
+func (n *NeuralNetwork) Train(input [][]float64, t [][]float64) {
+	for i := 0; i < n.epochs; i++ {
+		fmt.Printf("Epoch %v\n", i+1)
+		for i, arr := range input {
+			inputMatrix := NewColMatrix(arr)
+
+			// Calculate value of hidden nodes
+			hidden, err := MatrixProduct(n.weightsIH, inputMatrix)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			hidden.Map(n.activationFunc.f)
+
+			// Calculate values of output nodes
+			output, err2 := MatrixProduct(n.weightsHO, hidden)
+			if err2 != nil {
+				fmt.Println("Error: ", err2)
+				return
+			}
+			output.Map(n.activationFunc.f)
+
+			/*
+				Calculate the output error
+				Error = target - output
+			*/
+			target := NewColMatrix(t[i])
+			errorOutput, err3 := MatrixSub(target, output)
+			if err3 != nil {
+				fmt.Println("Error: ", err)
+				return
+			}
+
+			// We have errorOutput need to calculate gradient
+			// calculate grads for output
+			gradients := MatrixMap(output, n.activationFunc.df)
+			gradients.MulMat(errorOutput)
+			gradients.Mul(n.learningRate)
+
+			// calculate delta for weightsHO
+			hiddenT := MatrixTranspose(hidden)
+			weightsHODelta, err4 := MatrixProduct(gradients, hiddenT)
+			if err4 != nil {
+				fmt.Println("Error: ", err4)
+				return
+			}
+			n.weightsHO.AddMat(weightsHODelta) // update the weightsHO
+			n.biasHO.AddMat(gradients)         // updating the bias
+
+			/*
+				Calculate hidden error
+				Error = transpose(weightHO)*errorOutput
+			*/
+			weightsHOT := MatrixTranspose(n.weightsHO)
+			errorHidden, err5 := MatrixProduct(weightsHOT, errorOutput)
+			if err5 != nil {
+				fmt.Println("Error: ", err2)
+				return
+			}
+
+			// calculate hidden gradients
+			hiddenGradient := MatrixMap(hidden, n.activationFunc.df)
+			hiddenGradient.MulMat(errorHidden)
+			hiddenGradient.Mul(n.learningRate)
+
+			// calculate delta for weightsIH
+			inputMatrixT := MatrixTranspose(inputMatrix)
+			weightsIHDelta, err6 := MatrixProduct(hiddenGradient, inputMatrixT)
+			if err6 != nil {
+				fmt.Println("Error: ", err6)
+				return
+			}
+			n.weightsIH.AddMat(weightsIHDelta) // update the weightsIH
+			n.biasIH.AddMat(hiddenGradient)    // updating the bias
+		}
+	}
+}
+
+func (n *NeuralNetwork) Predict(input []float64) {
 	inputMatrix := NewColMatrix(input)
 
 	// Calculate value of hidden nodes
 	hidden, err := MatrixProduct(n.weightsIH, inputMatrix)
 	if err != nil {
 		fmt.Println(err)
-		return Matrix{}
+		return
 	}
 	hidden.Map(n.activationFunc.f)
 
@@ -73,42 +154,9 @@ func (n *NeuralNetwork) FeedForward(input []float64) Matrix{
 	output, err2 := MatrixProduct(n.weightsHO, hidden)
 	if err2 != nil {
 		fmt.Println("Error: ", err2)
-		return Matrix{}
+		return
 	}
 	output.Map(n.activationFunc.f)
-	return output
-}
-
-func (n *NeuralNetwork) BackPropagation(input []float64) {
-
-}
-
-func (n *NeuralNetwork) Train(input [][]float64, t [][]float64) {
-	for i, arr := range input {
-		output := n.FeedForward(arr)
-
-		/*
-			Calculate the output error
-			Error = target - output
-		*/
-		target := NewColMatrix(t[i])
-		errorOutput, err := MatrixSub(target, output)
-		if err != nil {
-			fmt.Println("Error: ", err)
-			return
-		}
-
-		/*
-			Calculate hidden error
-			Error = transpose(weightHO)*errorOutput
-		*/
-		weightsHOT := MatrixTranspose(n.weightsHO)
-		errorHidden, err2 := MatrixProduct(weightsHOT, errorOutput)
-		if err2 != nil {
-			fmt.Println("Error: ", err2)
-			return
-		}
-		errorHidden.Show()
-	}
+	output.Show()
 }
 
--- main.go
+++ main.go
@@ -4,9 +4,30 @@ import (
 	"github.com/sarthakpranesh/percepGo/cuslib"
 )
 
+var (
+	input = [][]float64 {{1, 1}, {0, 1}, {0, 0}, {1, 0}}
+	target = [][]float64 {{0}, {1}, {0}, {1}}
+)
+
 func main() {
-	input := [][]float64 {{1, 1}, {1, 0}, {0, 1}, {0, 0}}
-	target := [][]float64 {{0}, {1}, {1}, {0}}
-	nn := cuslib.NewNN(2, 2, 1, 1.6, "sgd")
+	/*
+			-	-	-	XOR Problem	-	-	-
+		Below is an example of a simple XOR problem solved using
+		cuslib Neural Network Library and Matrix Library.
+
+		Spoiler: Both the libraries are only for toying around. I
+		built them just for fun and learning GO programming. Feel
+		free to play around. Some important useful notes below
+
+		The Neural Network is essentially a three fully connected
+		layer network ( did not have the will to sit and code for
+		more dynamically given layer numbers ). You can read more
+		about it in "./cuslib/nn.go"
+	*/
+	nn := cuslib.NewNN(2, 10, 1, 1, "sgd", 500)
 	nn.Train(input, target)
+	nn.Predict([]float64 {1, 1})
+	nn.Predict([]float64 {0, 1})
+	nn.Predict([]float64 {1, 0})
+	nn.Predict([]float64 {0, 0})
 }
